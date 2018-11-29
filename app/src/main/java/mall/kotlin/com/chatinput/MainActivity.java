c6746ac0ba44974582ca7c217c237c1a4acd9759
Moon ch <changjunjie07@163.com>
2018-11-29 14:55:30 +0800

    输入框文本布局

--- app/src/main/java/mall/kotlin/com/chatinput/MainActivity.java
+++ app/src/main/java/mall/kotlin/com/chatinput/MainActivity.java
@@ -2,12 +2,16 @@ package mall.kotlin.com.chatinput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+//    @InjectView(R.id.input_panel)
+//    ChatInput mInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_main);
     }
 }
