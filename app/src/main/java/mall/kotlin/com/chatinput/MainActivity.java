dddd7e45247cdf56e5c13d868c72e5bc1defef56
Moon ch <changjunjie07@163.com>
2018-12-03 13:52:07 +0800

    commit

--- app/src/main/java/mall/kotlin/com/chatinput/MainActivity.java
+++ app/src/main/java/mall/kotlin/com/chatinput/MainActivity.java
@@ -1,12 +1,12 @@
 package mall.kotlin.com.chatinput;
 
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
-//    @InjectView(R.id.input_panel)
-//    ChatInput mInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +14,51 @@ public class MainActivity extends AppCompatActivity {
 //        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    /**
+     * @param root 最外层的View
+     * @param scrollToView 不想被遮挡的View,会移动到这个Veiw的可见位置
+     * autoScrollView(ll_loginView, btn_login);//弹出软键盘时滚动视图
+     */
+    private int scrollToPosition = 0;
+
+    private void autoScrollView(final View root, final View scrollToView) {
+        root.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+
+                        Rect rect = new Rect();
+
+                        //获取root在窗体的可视区域
+                        root.getWindowVisibleDisplayFrame(rect);
+
+                        //获取root在窗体的不可视区域高度(被遮挡的高度)
+                        int rootInvisibleHeight = root.getRootView().getHeight() - rect.bottom;
+
+                        //若不可视区域高度大于150，则键盘显示
+                        if (rootInvisibleHeight > 150) {
+
+                            //获取scrollToView在窗体的坐标,location[0]为x坐标，location[1]为y坐标
+                            int[] location = new int[2];
+                            scrollToView.getLocationInWindow(location);
+
+                            //计算root滚动高度，使scrollToView在可见区域的底部
+                            int scrollHeight = (location[1] + scrollToView.getHeight()) - rect.bottom;
+
+                            //注意，scrollHeight是一个相对移动距离，而scrollToPosition是一个绝对移动距离
+                            scrollToPosition += scrollHeight;
+
+                        } else {
+                            //键盘隐藏
+                            scrollToPosition = 0;
+                        }
+                        root.scrollTo(0, scrollToPosition);
+
+                    }
+                });
+    }
+
 }
